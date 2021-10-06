f5e50a58597ed07ccd05317258340800891b7257
Caleb Iuliano <calebiuliano@gmail.com>
2021-10-06 17:54:25 -0400

    finished quickSort

--- quickSort.js
+++ quickSort.js
@@ -1,4 +1,26 @@
+// Big-O:
+  // O(logn) on average
+    // More likely with a random pivot or pivot near the middle
+    // Choosing the first index is more likely to result in O(n2)
+  // O(n2) at worst
+    // Speed depends on pivot point
+      // Has a faster constant time than merge sort
+      // Merger sort is always O(logn) but
+       // at O(logn), quick sort is faster than merge sort
+
+
+
+// D&C (divide and conquer) strategy
+// Based off euclids Algorithm
+// Partitions data
+  // Sorts everything less than in a box to the left
+  // Sorts everything greater than in a box to the right
+    // Repeats process with each side
+    // Once the smallest box that works is found, you know what works for the entire list
+
+
 const quickSort = (arr) => {
+  // If only 2 items exist in array, we can sort it
   if (arr.length < 2) {
     return arr;
   } else {
@@ -6,6 +28,7 @@ const quickSort = (arr) => {
     let left = [];
     let right = [];
 
+    // Split array into lesser and greater sides
     for (let i = 0; i <= arr.length; i++) {
       if (arr[i] < arr[0]) {
         left.push(arr[i]);
@@ -15,6 +38,7 @@ const quickSort = (arr) => {
       }
     }
 
+    // Sort the left and right arrays
     let leftRes = quickSort(left);
     let rightRes = quickSort(right);
 
@@ -25,8 +49,11 @@ const quickSort = (arr) => {
       rightRes = [];
     }
 
+    // Combine left array with pivot point and right array
     leftRes.push(pivot);
     const result = leftRes.concat(rightRes);
+
+    // return sorted array
     console.log(result);
     return result;
   }
